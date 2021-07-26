b2ba0a37798910cbf258d8be080e866adf60b234
msindhuja29 <msindhuja18bcs@iiitkottayam.ac.in>
2021-07-26 19:18:01 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# COVID-19-Tracker
+COVID19 Tracker using React JS for tracking live cases for all courtiers.
+
+## Demo Link
+URL: https://covidreport19.netlify.app/
+
+
+## Features
+- Live Coronavirus Cases worldwide and other Countries
+- Live Coronavirus Recovered and Active Cases worldwide and other Countries
+- Statistics of Coronavirus Cases
+
+
+## Prerequisites
+
+Required to install and run the software:
+
+ * [npm](https://www.npmjs.com/get-npm)
+
+
+## APIs
+I used API from :- https://disease.sh/
+
+## MAP
+I used Map from https://www.openstreetmap.org/
+
+## Installing and Running
+
+From the project folder, run these commands in console (terminal) to install dependencies and run the app:
+```
+1. npm install
+2. npm start
+```
---
+++ build/precache-manifest.ec938a348cc6de03ed1b0c7fcf6aca74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a677aeae65391cf6e2ac48cc49f45d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08f8b127c9742b63495a",
+    "url": "/static/css/2.15f857eb.chunk.css"
+  },
+  {
+    "revision": "5add248d3bace6d09f3c",
+    "url": "/static/css/main.c505f358.chunk.css"
+  },
+  {
+    "revision": "08f8b127c9742b63495a",
+    "url": "/static/js/2.0d3c6206.chunk.js"
+  },
+  {
+    "revision": "88ab8e9bfa1a02a6d25f3aa28a50aeac",
+    "url": "/static/js/2.0d3c6206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5add248d3bace6d09f3c",
+    "url": "/static/js/main.8e26e88c.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
